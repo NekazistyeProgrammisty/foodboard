8a044285fea304819682ff75098f885c0dc436a1
fac3lessd0ge <garmasharseniy@gmail.com>
2022-03-30 00:03:37 +0300

    Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      Ну вощемт готово, в пятницу показываем Артёму
+    </div>
+  );
+}
+
+export default App;
