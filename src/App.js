7fcb50c49a65210a08a52b8eca6580ea348214ae
fac3lessd0ge <garmasharseniy@gmail.com>
2022-03-30 00:48:41 +0300

    Started working on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Ну вощемт готово, в пятницу показываем Артёму
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css'
+
+const Header = () => {
+    return (  
+        <header>
+            <span className='text-logo'>Рецепты</span>
+        </header>
+    );
+}
+ 
+export default Header;
