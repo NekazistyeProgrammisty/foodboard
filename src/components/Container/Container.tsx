ae73d870ba9425aca14450293f336945b14044d4
fac3lessd0ge <garmasharseniy@gmail.com>
2022-04-02 14:42:22 +0300

    grid cards + adaptive

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -8,18 +8,31 @@ interface ContainerProps {
 }
  
 const Container: React.FunctionComponent<ContainerProps> = () => {
-    return ( 
-        <>
-            
-            <div className='container'>
-                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]}/>
-                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]}/>
-                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]}/>
-                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]}/>
-                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]}/>
+    return (
+        <main className='container'>
+            <div className='grid-cardholder'>
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
+                <Card cardTitle='Блины с гавной' imgUrl='./bliny.png' ingredientMeter={[1, 2]} />
             </div>
-        </>
-        
+        </main>
     );
 }
  
