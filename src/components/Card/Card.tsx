4b71e1f0e590677e6d9fedf24c9344255fd63f60
fac3lessd0ge <garmasharseniy@gmail.com>
2022-04-06 18:45:33 +0300

    better (still bad) adaptivity + card description

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,25 +2,25 @@ import * as React from 'react';
 
 import './Card.css'
 import bliny2 from '../../images/bliny.png';
-import {ReactComponent as Grocery} from '../../images/Grocery.svg';
-import {ReactComponent as TurnAround} from '../../images/turnaround.svg';
+import { ReactComponent as Grocery } from '../../images/Grocery.svg';
+import { ReactComponent as TurnAround } from '../../images/turnaround.svg';
 import CardButton from '../CardButton/CardButton';
 import CardAvatar from '../CardAvatar/CardAvatar';
 
 //className={`card back ${!flipped ? '' : 'flipped'}`}
- 
+
 export interface CardProps {
-    imgUrl : string
-    title : string
-    ingredientMeter : number[]
+    imgUrl: string
+    title: string
+    ingredientMeter: number[]
 }
- 
-export const Card: React.FunctionComponent<CardProps> = (props:CardProps) => {
+
+export const Card: React.FunctionComponent<CardProps> = (props: CardProps) => {
     const [flipped, setFlipped] = React.useState<boolean>(false);
 
     const RETURN_TIMEOUT = 0;
 
-    const flipHandler = () : void => {
+    const flipHandler = (): void => {
         setFlipped(() => !flipped);
     }
 
@@ -48,8 +48,13 @@ export const Card: React.FunctionComponent<CardProps> = (props:CardProps) => {
                     </CardButton>
                 </div>
             </div>
-            <div className={`card back`}> 
-                <p className='card-title'>{props.title}</p>
+            <div className={`card back`}>
+                <div className="back-text-wrapper">
+                    <p className='card-back-title'>{props.title}</p>
+                    <div className='card-back-description'>
+                        Еслиб я имел коня - это был бы номер...<br></br> Еслиб конь имел меня - я б, наверно, помер...
+                    </div>
+                </div>
                 <div className='lower-card-container'>
                     <CardButton clickHandler={flipHandler} isSecond={true}>
                         <TurnAround />
@@ -57,8 +62,8 @@ export const Card: React.FunctionComponent<CardProps> = (props:CardProps) => {
                 </div>
             </div>
         </div>
-        
+
     );
 }
- 
+
 export default Card;
