908ef1cfb603bfc4ec85dde35622ae361b0463ec
fac3lessd0ge <garmasharseniy@gmail.com>
2022-04-15 16:27:25 +0300

    Refactoring. Starting product fields

---
+++ src/components/ProductElement/ProductElement.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { IProduct } from '../../types/ProductTypes';
+
+import classes from './ProductElement.module.css';
+
+const ProductElement: React.FunctionComponent<IProduct> = () => {
+	const aaa = 123;
+
+	return <div>О, привет, а я тя знаю))</div>;
+};
+
+export default ProductElement;
