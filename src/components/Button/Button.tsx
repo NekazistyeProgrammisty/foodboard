4b753a22ba5f0fa375b8be34ec87d34682741ccf
fac3lessd0ge <garmasharseniy@gmail.com>
2022-06-24 23:09:51 +0300

    CardHolder grid refactoring + Button rewrote with `styled-components`

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import './Button.css';
+import styled from 'styled-components';
 
 interface IButtonProps {
     text: string
     clickHandler?: React.MouseEventHandler<HTMLButtonElement> | undefined
 }
 
-const Button: React.FunctionComponent<IButtonProps> = ({ text, clickHandler }) => {
-	const buttonClass = 'button';
-	return (
-		<button
-			type="button"
-			className={buttonClass}
-			onClick={clickHandler}
-		>
-			{text}
-		</button>
-	);
-};
+const StyledButton = styled.button`
+	background-color: var(--primarycolor);
+    border: 0;
+    min-height: 2.2rem;
+    border-radius: 10px;
+    color: var(--txtcolor);
+    font-size: 0.95rem;
+    font-weight: 700;
+`;
+
+const Button: React.FunctionComponent<IButtonProps> = ({ text, clickHandler }) => (
+	<StyledButton
+		type="button"
+		className="button"
+		onClick={clickHandler}
+	>
+		{text}
+	</StyledButton>
+);
 
 export default Button;
