e73ba500435e59081601d6474ec6537c9e023574
fac3lessd0ge <garmasharseniy@gmail.com>
2022-06-28 18:27:44 +0300

    Footer recompose

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import './Footer.css';
-import FooterElement from '../FooterElement/FooterElement';
+import FooterElement from './FooterElement/FooterElement';
 import { ReactComponent as Heart } from '../../assets/images/footer/Heart.svg';
 import { ReactComponent as Products } from '../../assets/images/footer/Products.svg';
 import { ReactComponent as RecipeBook } from '../../assets/images/footer/RecipeBook.svg';
@@ -14,21 +14,17 @@ interface IFooterProps {
 
 const Footer: React.FC<IFooterProps> = ({ sections }) => (
 	<footer>
-		<FooterElement text={sections[0]}>
-			<Sparkles />
-		</FooterElement>
-		<FooterElement text={sections[1]}>
-			<Products />
-		</FooterElement>
-		<FooterElement text={sections[2]}>
-			<RecipeBook />
-		</FooterElement>
-		<FooterElement text={sections[3]}>
-			<User />
-		</FooterElement>
-		<FooterElement text={sections[4]}>
+		{[
+			<Sparkles />,
+			<Products />,
+			<RecipeBook />,
+			<User />,
 			<Heart />
-		</FooterElement>
+		].map((element, index) => (
+			<FooterElement text={sections[index]}>
+				{element}
+			</FooterElement>
+		))}
 	</footer>
 );
 
--- src/components/Footer/FooterElement/FooterElement.tsx
+++ src/components/Footer/FooterElement/FooterElement.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-import './FooterElement.css';
 
 interface FooterElementProps {
 	children: React.ReactNode;
 	text: string;
 }
 
+const StyledFooterElement = styled(Link)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.2rem;
+    color: var(--secondarycolor);
+    font-size: 0.7rem;
+    text-decoration: none;
+	cursor: pointer;
+`;
+
 const FooterElement: React.FunctionComponent<FooterElementProps> =
 	({ text, children }) => (
-		<Link
+		<StyledFooterElement
 			to={`/${text.toLowerCase().replace(' ', '')}`}
-			className="footer-element"
 		>
 			{children}
 			<span>{text}</span>
-		</Link>
+		</StyledFooterElement>
 	);
 
 export default FooterElement;
